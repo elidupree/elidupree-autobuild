c46f137e98fca19eda4818c75eb4699b337dfb7d
Eli Dupree <vcs@elidupree.com>
2019-11-02 14:40:12 -0400

    don't assume that everything is a rust program quite as hard

--- init_linux.sh
+++ init_linux.sh
@@ -3,6 +3,6 @@ mkdir /n/autobuild/$1
 mkdir /n/autobuild/$1/scripts
 mkdir /n/autobuild/$1/build
 printf "#!/bin/bash\n/n/elidupree-autobuild/watch.py \"\$(cat /n/elidupree-autobuild/share_prefix)\"/$1/modify_noticer /n/autobuild/$1/scripts/build.sh 1000000" > /n/autobuild/$1/scripts/watch.sh
-printf "#!/bin/bash\n/n/elidupree-autobuild/copy_source.sh \"\$(cat /n/elidupree-autobuild/share_prefix)\"/$1/ /n/autobuild/$1/build\ncd /n/autobuild/$1/build\nexport RUST_BACKTRACE=1\ncargo update && cargo run" > /n/autobuild/$1/scripts/build.sh
+printf "#!/bin/bash\n/n/elidupree-autobuild/copy_source.sh \"\$(cat /n/elidupree-autobuild/share_prefix)\"/$1/ /n/autobuild/$1/build\ncd /n/autobuild/$1/build\nexport RUST_BACKTRACE=1\n\n#cargo update && cargo run" > /n/autobuild/$1/scripts/build.sh
 chmod +x /n/autobuild/$1/scripts/watch.sh
 chmod +x /n/autobuild/$1/scripts/build.sh
