75302c9c595c0636b749ac4f4279cbe95294f022
Eli Dupree <vcs@elidupree.com>
2023-11-05 10:17:06 -0500

    old copy_source updates

--- copy_source.sh
+++ copy_source.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av --del --exclude ".git" --exclude "target/" --exclude "build/" --exclude "temp-hilariou*" --exclude "modify_noticer" --exclude "Cargo.lock" --exclude "package-lock.json" --exclude "proptest-regressions/" --exclude "callgrind.out.*" --exclude "node_modules/" $1 $2
+rsync -av --del --exclude ".git" --exclude "target/" --exclude ".embuild/" --exclude "build/" --exclude "temp-hilariou*" --exclude "modify_noticer" --exclude "Cargo.lock" --exclude "package-lock.json" --exclude "proptest-regressions/" --exclude "callgrind.out.*" --exclude "node_modules/" --exclude ".idea" "$@"
